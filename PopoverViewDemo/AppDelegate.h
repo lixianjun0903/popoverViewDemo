d71f407fdfc1bc19404c86f09daccaf21c7f54a5
guojiang <eventorlimited@jam-Mac-mini.local>
2014-09-04 17:28:08 +0800

    Initial Commit

---
+++ PopoverViewDemo/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  PopoverViewDemo
+//
+//  Created by guojiang on 4/9/14.
+//  Copyright (c) 2014年 guojiang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ PopoverViewDemo/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  PopoverViewDemo
+//
+//  Created by guojiang on 4/9/14.
+//  Copyright (c) 2014年 guojiang. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
